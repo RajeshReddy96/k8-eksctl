52c4a6ad35be7b820cad84c5b2fbc4791ba55b2f
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-11-05 20:41:46 +0530

    k8 install

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-055325cf6e3747df0"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0e2ad3470fb61c88d"
+}
